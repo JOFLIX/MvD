76321dc76ce5436869afd9fb5bd2ae24683e56ba
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-30 19:33:36 +0300

    implemented the authenticate functionality

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Shell, Server
 from app.models import User,Role
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
@@ -10,10 +11,21 @@ manager.add_command('server', Server)
 
 # manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+#db instance
 
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User,Role = Role )
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(testsg)
 if __name__ == '__main__':
     manager.run()
-
+    
