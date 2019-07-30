2d0036c0806f6c328e1ade3007da631a7630627a
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-30 18:43:56 +0300

    implemented the user and their roles in my database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+
+db = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -13,6 +16,7 @@ def create_app(config_name):
     config_options[config_name].init_app(app)
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Will add the views and forms
     
