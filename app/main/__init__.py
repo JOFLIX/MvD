52da420a363ba7030968bf413bc1dc72ef61d3e7
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-07-31 11:18:22 +0300

    implemented upload images into my db

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
 main = Blueprint('main',__name__)
 from . import views,error
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
+
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
