46d6df8c34a70f0c2b4e93d148ccd0db2becba8e
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-01 08:04:03 +0300

    ran the tests but failed

--- app/models.py
+++ app/models.py
@@ -4,17 +4,20 @@ from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class Movie:
     '''
     Movie class to define Movie Objects
     '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
+    def __init__(self, id, title, overview, poster, vote_average, vote_count):
+        self.id = id
         self.title = title
         self.overview = overview
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
@@ -22,64 +25,57 @@ class Movie:
         self.vote_count = vote_count
 
 
+class Review:
 
-class Review(db.Model):
+    all_reviews = []
 
-    __tablename__ = 'reviews'
-
-    id = db.Column(db.Integer,primary_key = True)
-    movie_id = db.Column(db.Integer)
-    movie_title = db.Column(db.String)
-    image_path = db.Column(db.String)
-    movie_review = db.Column(db.String)
-    posted = db.Column(db.Time,default=datetime.utcnow())
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-
+    def __init__(self, movie_id, title, imageurl, review):
+        self.movie_id = movie_id
+        self.title = title
+        self.imageurl = imageurl
+        self.review = review
 
     def save_review(self):
-        db.session.add(self)
-        db.session.commit()
-
+        Review.all_reviews.append(self)
 
     @classmethod
-    def get_reviews(cls,id):
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
 
-        reviews = Review.query.filter_by(movie_id=id).all()
-        return reviews
+    @classmethod
+    def get_reviews(cls, id):
 
+        response = []
 
-class PhotoProfile(db.Model):
-    __tablename__ = 'profile_photos'
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
 
-    id = db.Column(db.Integer,primary_key = True)
-    pic_path = db.Column(db.String())
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+        return response
 
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
-
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    password_hash = db.Column(db.String(255))
-    photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+    pass_secure = db.Column(db.String(255))
     reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
-
     @property
     def password(self):
-        raise AttributeError('You cannnot read the password attribute')
+        raise AttributeError('You cannot read the password attribute')
 
     @password.setter
     def password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.pass_secure = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.password_hash,password)
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
@@ -92,6 +88,25 @@ class Role(db.Model):
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic")
 
-
     def __repr__(self):
         return f'User {self.name}'
+    
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer,primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    
+    def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reviews(cls,id):
+        reviews = Review.query.filter_by(movie_id=id).all()
+        return reviews
