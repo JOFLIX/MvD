20283b54549fa27186c1e21339e9f526fd47ee43
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-01 15:48:27 +0300

    still deploying

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ export SECRET_KEY='<Flask WTF Secret Key>'
 export MAIL_USERNAME='emmahg6@gmail.com'
 export MAIL_PASSWORD='Emmanuel@71270706915605'
 python3.6 manage.py server
+
+# heroku config:set MOVIE_API_KEY='e2ae0ae98e1deb7064cfd586d964ce49' && heroku config:set SECRET_KEY='<Flask WTF Secret Key>' && heroku config:set MAIL_USERNAME='emmahg6@gmail.com' && heroku config:set MAIL_PASSWORD='Emmanuel@71270706915605'
